ddd12d6ce23b8204fc63d274577b8219aa49cb4f
TXTT <txtt2016@outlook.com>
2019-09-03 15:28:08 +0800

    no message

--- C++/排序/选择排序.cpp
+++ C++/排序/选择排序.cpp
@@ -27,10 +27,50 @@ void selsort(Elem A[], int len)
 				swap(A[i], A[j]);
 }
 
+void select_Sort(vector<int>& vec, int len)
+{
+	int minPos = 0;
+	for (int i = 0; i < len - 1;)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (vec[minPos] > vec[j])
+				minPos = j;
+		}
+		swap(vec[i], vec[minPos]);
+		minPos = ++i;
+	}
+}
+
+//选择排序优化方法，在每一次的选择中选出最大和最小的两个元素的位置，比原本的选择排序减少了一半循环次数。
+void select_Sort_2(vector<int>& vec, int len)
+{
+	int minPos = 0, maxPos = 0;
+	int right = len;
+	for (int i = 0; i < right - 1;)
+	{
+		for (int j = i + 1; j < right; j++)
+		{
+			if (vec[minPos] > vec[j])
+				minPos = j;
+			if (vec[maxPos] < vec[j])
+				maxPos = j;
+		}
+		if (maxPos != right)
+			swap(vec[maxPos], vec[right]);
+		if (minPos == right)
+			minPos = maxPos;
+		if (minPos != i)
+			swap(vec[minPos], vec[i]);
+		minPos = ++i;
+		maxPos = --right;
+	}
+}
+
 int main()
 {
 	vector<int> vec{ 20, 39, 12, 55, 34, 78, 56, 34 };
-	
+	select_Sort_2(vec, vec.size() - 1);
 
 	return 0;
 }
